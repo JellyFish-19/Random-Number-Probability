7f2f6ef43d0dfff12863975753c17caee697d501
JellyFish-19 <justinas.cepas19@gmail.com>
2020-05-30 14:22:45 +0300

    Uploaded

---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            for (int x = 1; x <= 100000000; x++)
+            {
+                Random random = new Random();
+                int diceOne = random.Next(1, 100);
+                int diceTwo = random.Next(1, 100);
+                int diceThree = random.Next(1, 100);
+
+                Console.WriteLine(diceOne);
+                Console.WriteLine(diceTwo);
+                Console.WriteLine(diceThree);
+
+                if (diceOne != diceTwo && diceOne != diceThree && diceTwo != diceThree)
+                {
+                    count++;
+                }
+
+                else if (diceOne == diceTwo && diceOne == diceThree && diceTwo == diceThree)
+                {
+                    Console.WriteLine("It's a match! It took " + count + " times!");
+                    break;
+                }
+            }
+        }
+    }
+}
